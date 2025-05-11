ae00f2f81fe2e592162be8db71b884e252126b88
SS <andyxvuitton@gmail.com>
2025-05-10 21:45:47 -0700

    modified:   css/styles.css 	modified:   index.html 	new file:   js/packagesPopUp.js

---
+++ js/packagesPopUp.js
@@ -0,0 +1,9 @@
+// Function to show the popup
+function showPopup() {
+  document.getElementById('bookAppointment').style.display = 'flex';
+}
+
+// Function to close the popup
+function closePopup() {
+  document.getElementById('bookAppointment').style.display = 'none';
+}
